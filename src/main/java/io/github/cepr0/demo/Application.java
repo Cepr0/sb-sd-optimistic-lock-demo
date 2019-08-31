daf114edb884b4434e22c49d1bb91a3431d3cc4a
Sergei Poznanski <cepr0@ukr.net>
2019-09-01 01:06:20 +0300

    Add logging

--- src/main/java/io/github/cepr0/demo/Application.java
+++ src/main/java/io/github/cepr0/demo/Application.java
@@ -1,11 +1,13 @@
 package io.github.cepr0.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.annotation.Order;
 
+@Slf4j
 @SpringBootApplication
 public class Application {
 
@@ -24,18 +26,21 @@ public class Application {
 	@Order(1)
 	@EventListener(ApplicationReadyEvent.class)
 	public void create() {
+		log.info("[i] Creating...");
 		model = modelService.create(new Model().setName("model"));
 	}
 
 	@Order(2)
 	@EventListener(ApplicationReadyEvent.class)
 	public void update() {
+		log.info("[i] Updating...");
 		modelService.update(model.getId(), new Model().setName("model_updated"));
 	}
 
 	@Order(3)
 	@EventListener(ApplicationReadyEvent.class)
 	public void getAll() {
+		log.info("[i] Getting...");
 		modelService.getAll().forEach(System.out::println);
 	}
 }
